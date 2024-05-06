3e3270e279ae21ccd27303a0c7892635a7f22e20
akermen <akermen@gmail.com>
2024-05-06 09:59:11 +0300

    Add snippets plugin

---
+++ lua/custom/plugins/friendly-snippets.lua
@@ -0,0 +1,7 @@
+-- https://github.com/rafamadriz/friendly-snippets
+return {
+  'rafamadriz/friendly-snippets',
+  config = function()
+    require('luasnip.loaders.from_vscode').lazy_load()
+  end,
+}
