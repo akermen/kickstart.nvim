970595054faadba5b0591fdcdc2b366aefe57d02
akermen <akermen@gmail.com>
2025-11-15 13:52:33 +0300

    update gen-nvim ai model

--- lua/custom/plugins/gen-nvim.lua
+++ lua/custom/plugins/gen-nvim.lua
@@ -2,7 +2,7 @@
 return {
   'David-Kunz/gen.nvim',
   opts = {
-    model = 'mistral', -- The default model to use.
+    model = 'deepseek-r1', -- The default model to use.
     -- model = 'llama3', -- The default model to use.
     host = 'localhost', -- The host running the Ollama service.
     port = '11434', -- The port on which the Ollama service is listening.
